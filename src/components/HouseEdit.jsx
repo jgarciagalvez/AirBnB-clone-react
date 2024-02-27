d52a43ded24a07da43cc2e85cfb989689af475ed
Jonathan Centeno <148176416+Joncen88@users.noreply.github.com>
2024-02-27 14:31:31 +0700

    updated to houseedit

--- src/components/HouseEdit.jsx
+++ src/components/HouseEdit.jsx
@@ -1,9 +1,9 @@
 function HouseEdit() {
   return (
     <div>
-      <div className="grid gap-4 grid-cols-5 grid-rows-3">
-        <div>1</div>
-        <div>2</div>
+      <div className="grid gap-10 grid-cols-2 grid-rows-1 bg-pink-200">
+        <div className="bg-pink-500">1</div>
+        <div className="bg-pink-500">2</div>
       </div>
     </div>
   )
--- src/components/Houses.jsx
+++ src/components/Houses.jsx
@@ -2,22 +2,22 @@ function Houses() {
   return (
     <div className="container mx-auto">
       {/* searches for houses */}
-      <div className="flex gap-4 grid-cols-5 grid-rows-3">
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
-        <div>4</div>
-        <div>5</div>
-        <div>6</div>
+      <div className="flex justify-between gap-1 grid-cols-5 grid-rows-1 bg-sky-200">
+        <div className="bg-sky-500">1</div>
+        <div className="bg-sky-500">2</div>
+        <div className="bg-sky-500">3</div>
+        <div className="bg-sky-500">4</div>
+        <div className="bg-sky-500">5</div>
+        <div className="bg-sky-500">6</div>
       </div>
-      <div className="grid gap-4 grid-cols-5 grid-rows-3">
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
-        <div>4</div>
-        <div>5</div>
-        <div>6</div>
-        <div>7</div>
+      <div className="grid gap-4 grid-cols-5 grid-rows-2 bg-pink-200">
+        <div className="bg-pink-500">1</div>
+        <div className="bg-pink-500">2</div>
+        <div className="bg-pink-500">3</div>
+        <div className="bg-pink-500">4</div>
+        <div className="bg-pink-500">5</div>
+        <div className="bg-pink-500">6</div>
+        <div className="bg-pink-500">7</div>
       </div>
     </div>
   )
