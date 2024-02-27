54b890c5107642ee4db8b10d81740f2f5da86de6
Jonathan Centeno <148176416+Joncen88@users.noreply.github.com>
2024-02-27 17:04:08 +0700

    house.jsx

--- src/components/HouseEdit.jsx
+++ src/components/HouseEdit.jsx
@@ -1,7 +1,7 @@
 function HouseEdit() {
   return (
     <div>
-      <div className="grid gap-10 grid-cols-2 grid-rows-1 bg-pink-200">
+      <div className="grid gap-10 grid-cols-2 grid-rows-1  bg-pink-200">
         <div className="bg-pink-500">1</div>
         <div className="bg-pink-500">2</div>
       </div>
