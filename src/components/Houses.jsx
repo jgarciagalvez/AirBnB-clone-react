75a6fcb1e937f2781cf7f5ab649f9f6a428616d4
Jonathan Centeno <148176416+Joncen88@users.noreply.github.com>
2024-02-27 13:54:43 +0700

    grid and flexbox

--- src/components/Houses.jsx
+++ src/components/Houses.jsx
@@ -1,7 +1,24 @@
 function Houses() {
   return (
-    <div>
-      <p>Houses pages</p>
+    <div className="container mx-auto">
+      {/* searches for houses */}
+      <div className="flex">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+        <div>5</div>
+        <div>6</div>
+      </div>
+      <div className="grid gap-4 grid-cols-5 grid-rows-3">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+        <div>5</div>
+        <div>6</div>
+        <div>7</div>
+      </div>
     </div>
   )
 }
