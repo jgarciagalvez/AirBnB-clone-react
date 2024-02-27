0fe6d0f91ee36919d6c0d8818dd94223ed8a98b9
therosstar <123678594+therosstar@users.noreply.github.com>
2024-02-27 05:34:52 +0000

    Ross/ added listings and profile routes

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,12 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import Home from './components/Home'
+import Houses from './components/Houses'
+import Profile from './components/Profile'
+import Listings from './components/Listings'
 
 function Router() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path='/' element={<Home />} />
+                <Route path='/' element={<Houses />} />
+                <Route path='/profile' element={<Profile />} />
+                <Route path='/listings' element ={<Listings />} />
             </Routes>
         </BrowserRouter>
     )
---
+++ src/components/Profile.jsx
@@ -0,0 +1,10 @@
+function profile() {
+    return (
+        <div>
+            <p>Hello from profile!</p>
+        </div>
+    )
+
+}
+
+export default profile
