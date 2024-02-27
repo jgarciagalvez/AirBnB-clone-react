cf48f8018a9cbc7db6d92499d24c7a1c48783e0b
therosstar <123678594+therosstar@users.noreply.github.com>
2024-02-27 07:22:03 +0000

    created profile flex layout

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,10 +1,10 @@
 function profile() {
-    return (
-        <div>
-            <p>Hello from profile!</p>
-        </div>
-    )
-
+  return (
+    <div className="flex justify-start gap-2 bg-blue-200">
+      <div className="bg-blue-600">1</div>
+      <div className="bg-blue-600">2</div>
+    </div>
+  )
 }
 
 export default profile
