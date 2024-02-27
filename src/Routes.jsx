02eb3686bbee4ca1223adcc8e40b44caae62251c
Jesus <40635808+jgarciagalvez@users.noreply.github.com>
2024-02-27 15:37:52 +0700

    Create dummy component for Login

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -5,21 +5,23 @@ import Profile from './components/Profile'
 import Listings from './components/Listings'
 import HouseEdit from './components/HouseEdit'
 import Signup from './components/Signup'
+import Login from './components/Login'
 
 function Router() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<Houses />} />
-                <Route path='/profile' element={<Profile />} />
-                <Route path='/listings' element ={<Listings />} />
-                <Route path='/houses/:id' element={<Houses />} />
-                <Route path='/houses' element={<Houses />} />
-                <Route path='/houses/:id/edit' element={<HouseEdit />} />
-                <Route path='/signup' element={<Signup />} />
-            </Routes>
-        </BrowserRouter>
-    )
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Houses />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/listings" element={<Listings />} />
+        <Route path="/houses/:id" element={<Houses />} />
+        <Route path="/houses" element={<Houses />} />
+        <Route path="/houses/:id/edit" element={<HouseEdit />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default Router
