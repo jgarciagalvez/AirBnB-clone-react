eb87fc22fcff60475452cbce0008d0a7e31dd021
Jonathan Centeno <148176416+Joncen88@users.noreply.github.com>
2024-02-27 12:31:48 +0700

    created houses and house edit jsx

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,12 +1,17 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import Home from './components/Home'
+import Houses from './components/Houses'
+import HouseEdit from './components/HouseEdit'
 
 function Router() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<Home />} />
+                <Route path='/houses/:id' element={<Houses />} />
+                <Route path='/houses' element={<Houses />} />
+                <Route path='/houses/:id/edit' element={<HouseEdit />} />
             </Routes>
         </BrowserRouter>
     )
