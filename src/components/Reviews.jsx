bd5d8dfafb485e0fc7b491d483b62e085aff1cac
Jonathan Centeno <148176416+Joncen88@users.noreply.github.com>
2024-02-28 11:47:47 +0700

    updated reviews

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,8 +1,12 @@
+import Nav from './Nav'
 export default function Reviews() {
   return (
-    <div className="flex grid-cols-2 grid-rows-1 gap-2 bg-sky-200">
-      <Review />
-      <Review />
+    <div className="container mx-auto">
+      <Nav />
+      <div className="flex grid-cols-2 grid-rows-1 gap-2 bg-sky-200">
+        <Review />
+        <Review />
+      </div>
     </div>
   )
 }
