c94d47e475cd1aec305476d3e95f7eb8dc592300
Jesus <40635808+jgarciagalvez@users.noreply.github.com>
2024-02-28 12:57:10 +0700

    Create Base Stilying for reviews

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,9 +1,17 @@
-import Nav from './Nav'
+import IconChatbubbleEllipsesOutline from './icons/reviewsIcon'
+
 export default function Reviews() {
   return (
-    <div className="container mx-auto">
-      <Nav />
-      <div className="flex grid-cols-2 grid-rows-1 gap-2 bg-sky-200">
+    <div className="container mx-auto gap-2">
+      <div className="flex gap-2 items-center">
+        <div className="inline">
+          <IconChatbubbleEllipsesOutline />
+        </div>
+        <div className="inline">
+          <span className="text-lg font-bold">34 Reviews</span>
+        </div>
+      </div>
+      <div className="space-y-3">
         <Review />
         <Review />
       </div>
@@ -12,5 +20,36 @@ export default function Reviews() {
 }
 
 function Review() {
-  return <div className="bg-sky-500">Review</div>
+  return (
+    <div className="border border-solid border-[#E5E7EB] rounded-lg p-3 m-2 text-sm">
+      <div className="flex gap-2">
+        <div className="w-12 h-12 border-2 border-solid border-white rounded-full">
+          IMG
+        </div>
+        <div>
+          <div className="text-[#64748B]">5 January 2024</div>
+          <div>Sandra Berger</div>
+        </div>
+      </div>
+      <div className="flex gap-2 my-2">
+        <div>Stars</div>
+        <div>4.7</div>
+      </div>
+      <div className="flex">
+        <p>
+          On the positive side, the location was fantastic - just a short walk
+          to the beach and local eateries. The view from the balcony was
+          breathtaking, especially during sunsets. However, the house itself
+          left a bit to be desired. The decor was charming and beach-themed, but
+          the cleanliness was not up to par; we found dust in several corners
+          and the bathroom fixtures were a bit dated. The amenities listed were
+          all present, but the Wi-Fi was frustratingly slow and unreliable.
+          Additionally, while the host was polite, their response time was
+          slower than expected, which made coordinating check-in more
+          challenging than it needed to be. It's a decent place if you're not
+          too fussy about the finer details and are more
+        </p>
+      </div>
+    </div>
+  )
 }
