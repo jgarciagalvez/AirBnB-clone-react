bcd4b72ca01756965b3e890fba1999c5ce2b0f96
Jonathan Centeno <148176416+Joncen88@users.noreply.github.com>
2024-02-28 13:30:13 +0700

    Update HouseEdit.jsx

--- src/components/HouseEdit.jsx
+++ src/components/HouseEdit.jsx
@@ -3,7 +3,7 @@ function HouseEdit() {
   return (
     <div className="container mx-auto">
       <Nav />
-      <div className="grid gap-10 grid-cols-2 grid-rows-1  bg-pink-200">
+      <div className="grid gap-10 grid-cols-2 grid-rows-1  ">
         <div>1</div>
         <div>2</div>
       </div>
