2b0fb72806d6d2ff0d0fff5aad0417d418d6d4a1
Jonathan Centeno <148176416+Joncen88@users.noreply.github.com>
2024-03-01 11:42:36 +0700

    Update Signup.jsx

--- src/components/pages/Signup.jsx
+++ src/components/pages/Signup.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 
 function signup() {
   const classNameInput = 'border border-[#E5E7EB] rounded-md h-10 pl-4'
-  const classNameLabel = 'text-[#64748B] pb-1 mt-2 decoration-0'
+  const classNameLabel = 'text-[#64748B] pb-1 mt-2 font-extralight'
   return (
     <div className="container mx-auto flex justify-center">
       <div className="border border-[#E5E7EB] shadow-md rounded-lg p-4 mt-4 w-[320px]">
@@ -35,7 +35,7 @@ function signup() {
           </form>
         </div>
         <div className="ml-1 text-sm">
-          <p>
+          <p className="font-extralight">
             Already have an account?
             <Link
               to="/login"
