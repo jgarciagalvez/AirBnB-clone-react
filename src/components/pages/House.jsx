7726e2b7c692c6bce8f1eb194b2953abcc84f3e3
Jonathan Centeno <148176416+Joncen88@users.noreply.github.com>
2024-03-06 08:30:18 +0700

    updated galleries

--- src/components/pages/House.jsx
+++ src/components/pages/House.jsx
@@ -48,13 +48,8 @@ function House() {
       <div className="flex flex-col p-4 gap-5">
         {/* Images on page */}
 
-        <div className="grid sm:grid-cols-2 gap-2">
-          {/* Highlight Image */}
-          <div className="rounded-md md:min-h-[200px] sm:min-h-[100px] bg-cover bg-center bg-[url('https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295026/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_01.png')]"></div>
-
-          {/* Gallery */}
-          <Gallery />
-        </div>
+        {/* Gallery */}
+        <Gallery />
 
         {/* House Info & Booking */}
 
--- src/components/ui/Gallery.jsx
+++ src/components/ui/Gallery.jsx
@@ -1,60 +1,17 @@
-export default function Gallery() {
+export default function Gallery({ images }) {
   const imgStyle = 'hover:shadow-md rounded-md h-auto'
   return (
-    <div className="grid gap-1.5 md:grid-cols-3 sm:grid-cols-2">
-      <img
-        src="https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295026/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_01.png"
-        alt="Living Room"
-        className={imgStyle}
-      />
-
-      <img
-        src="https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295026/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_02.png"
-        alt=""
-        className={imgStyle}
-      />
-
-      <img
-        src="https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295026/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_03.png"
-        alt=""
-        className={imgStyle}
-      />
-
-      <img
-        src="https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295026/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_04.png"
-        alt=""
-        className={imgStyle}
-      />
-
-      <img
-        src="https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295026/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_05.png"
-        alt=""
-        className={imgStyle}
-      />
-
-      <img
-        src="https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295026/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_06.png"
-        alt=""
-        className={imgStyle}
-      />
-
-      <img
-        src="https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295026/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_07.png"
-        alt=""
-        className={imgStyle}
-      />
-
-      <img
-        src="https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295026/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_08.png"
-        alt=""
-        className={imgStyle}
-      />
-
-      <img
-        src="https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295026/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_09.png"
-        alt=""
-        className={imgStyle}
-      />
+    <div className="grid sm:grid-cols-2 gap-2">
+      {/* Featured Picture */}
+      <div className="rounded-md md:min-h-[200px] sm:min-h-[100px] bg-cover bg-center bg-[url('https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295026/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_01.png')]">
+        <img scr={[0]} alt="" />
+      </div>
+      {/* Gallery Pictures*/}
+      <div className="grid gap-1.5 md:grid-cols-3 sm:grid-cols-2">
+        {images.map((image, i) => (
+          <img scr={image} key={i} alt="" />
+        ))}
+      </div>
     </div>
   )
 }
