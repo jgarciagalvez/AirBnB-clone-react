dcc6a10d8b65b448bd0ed3417415ca4e58a32c24
Jesus <40635808+jgarciagalvez@users.noreply.github.com>
2024-03-08 17:21:39 +0700

    Solve Merge Conflict with main

--- src/components/pages/HouseEdit.jsx
+++ src/components/pages/HouseEdit.jsx
@@ -9,14 +9,14 @@ function HouseEdit() {
   const classNameInput = 'border border-[#E5E7EB] rounded-sm'
   const classNameLabel = 'text-[#64748B]'
 
-  const getData = async () => {
-    const response = await axios.get(
-      'https://haiku-bnb.onrender.com/houses/' + id
-    )
-    setHouse(response.data)
-  }
-
   useEffect(() => {
+    const getData = async () => {
+      const response = await axios.get(
+        'https://haiku-bnb.onrender.com/houses/' + id
+      )
+      setHouse(response.data)
+    }
+
     getData()
   }, [id])
 
