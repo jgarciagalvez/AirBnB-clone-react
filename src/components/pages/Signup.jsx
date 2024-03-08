5f5434fb354fc396948858230d8a4c29f811b252
Jonathan Centeno <148176416+Joncen88@users.noreply.github.com>
2024-03-08 13:56:31 +0700

    Update Signup.jsx

--- src/components/pages/Signup.jsx
+++ src/components/pages/Signup.jsx
@@ -1,8 +1,27 @@
-import { Link } from 'react-router-dom'
-
+import { Link, useNavigate } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 function signup() {
   const classNameInput = 'border border-[#E5E7EB] rounded-md h-10 pl-4'
   const classNameLabel = 'text-[#64748B] pb-1 mt-2 font-extralight'
+  const [error, setError] = useState('')
+  const navigate = useNavigate()
+
+  const submitForm = async (e) => {
+    e.preventDefault()
+    console.log(e.target.email.value)
+    console.log(e.target.password.value)
+
+    const response = await axios.post('https://haiku-bnb.onrender.com/signup', {
+      email: e.target.email.value,
+      password: e.target.password.value
+    })
+    if (response.data.error) {
+      setError(response.data.error)
+    } else {
+      navigate('/')
+    }
+  }
   return (
     <div className="container mx-auto flex justify-center">
       <div className="border border-[#E5E7EB] shadow-md rounded-lg p-4 mt-4 w-[320px]">
@@ -14,18 +33,20 @@ function signup() {
           />
         </div>
         <div className="p-1">
-          <form className="flex flex-col">
+          <form onSubmit={submitForm} className="flex flex-col">
             <label className={classNameLabel}>First Name</label>
-            <input type="text" className={classNameInput} />
+            <input type="text" name="firstName" className={classNameInput} />
             <label className={classNameLabel}>Last Name</label>
-            <input type="text" className={classNameInput} />
+            <input type="text" name="lastName" className={classNameInput} />
             <label className={classNameLabel}>Email</label>
-            <input type="email" className={classNameInput} />
+            <div className="text-red-500">{error}</div>
+            <input type="email" name="email" className={classNameInput} />
             <label className={classNameLabel}>Password</label>
-            <input type="password" className={classNameInput} />
+            <input type="password" name="password" className={classNameInput} />
             <label className={classNameLabel}>Profile Picture</label>
             <input
               type="text"
+              name="profile"
               placeholder="https://..."
               className={classNameInput}
             />
