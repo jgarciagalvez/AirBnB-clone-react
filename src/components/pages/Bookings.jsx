0ee94763f0bfedce3123761021d1c566d296fd7c
Jesus <40635808+jgarciagalvez@users.noreply.github.com>
2024-03-25 15:52:25 +0700

    Fetch Bookings Data from API

--- src/components/pages/Bookings.jsx
+++ src/components/pages/Bookings.jsx
@@ -1,24 +1,45 @@
 import Nav from '../ui/Nav'
 import HouseCard from '../ui/HouseCard'
-import { bookings } from '../../dummyData.js'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
 
 function Bookings() {
-  // Loop through the bookings array and render a HouseCard component for
-  //each, replace the hardcoded values in the UI with data coming from each object
+  // Define useState Variables
+  const [bookings, setBookings] = useState([])
+
+  // Get Listings Data from API
+  const getBookings = async () => {
+    let { data } = await axios.get('https://haiku-bnb.onrender.com/bookings')
+    // Verify if 'data' is an array before setting 'bookings'. This is crucial because 'bookings.map' is used later in the code.
+
+    Array.isArray(data) && setBookings(data)
+  }
+
+  useEffect(() => {
+    getBookings()
+  }, [])
 
   return (
     <div className="container mx-auto">
       <Nav />
-      <div className="grid md:grid-cols-5 gap-3 sm:grid-cols-2">
-        {bookings.map((booking) => (
-          <HouseCard
-            house={booking.house}
-            key={booking.booking_id}
-            booking={booking}
-            isBooking={true}
-          />
-        ))}
-      </div>
+      {bookings && bookings.length > 0 ? (
+        <div className="grid md:grid-cols-5 gap-3 sm:grid-cols-2">
+          {bookings.map((booking, id) => (
+            <HouseCard
+              house={booking.house}
+              key={id}
+              booking={booking}
+              isBooking={true}
+            />
+          ))}
+        </div>
+      ) : (
+        <div className="flex items-center justify-center h-64">
+          <span className="text-xl font-semibold">
+            You don't have any bookings
+          </span>
+        </div>
+      )}
     </div>
   )
 }
