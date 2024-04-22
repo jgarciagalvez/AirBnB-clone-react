fd24fc58c01e812fe89a9dfe0a7158cdd6aa032b
Jesus <40635808+jgarciagalvez@users.noreply.github.com>
2024-04-22 10:39:57 +0800

    Make Login & Signup responsives

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,6 +1,7 @@
 import { Link, useNavigate } from 'react-router-dom'
 import { useState } from 'react'
 import axios from 'axios'
+import Nav from '../ui/Nav'
 
 axios.defaults.withCredentials = true
 
@@ -40,44 +41,47 @@ function Login() {
 
   // JSX
   return (
-    <div className="flex mx-auto justify-center m-5">
-      <div className="border-2 p-6 rounded-md w-72">
-        <div className="flex justify-center">
-          <img
-            className="h-12"
-            src="https://www.vectorlogo.zone/logos/airbnb/airbnb-ar21.svg"
-            alt="AirBnB Logo"
-          />
-        </div>
-
-        <form onSubmit={submitForm} className="grid gap-3">
-          <div>
-            <label className="text-stone-500 text-sm">Email:</label>
-            <input type="text" name="email" className="border w-full p-1" />
-          </div>
-          <div>
-            <label className="text-stone-500 text-sm">Password:</label>
-            <input
-              type="password"
-              name="password"
-              className="border w-full p-1"
+    <div className="container mx-auto">
+      <Nav />
+      <div className="flex mx-auto justify-center">
+        <div className="border-2 p-6 rounded-md md:w-1/3 m-3">
+          <div className="flex justify-center">
+            <img
+              className="h-12"
+              src="https://www.vectorlogo.zone/logos/airbnb/airbnb-ar21.svg"
+              alt="AirBnB Logo"
             />
           </div>
-          <button className=" bg-[#FF5A5F] border text-white p-2 rounded-md w-full">
-            Login
-          </button>
-          <div className="text-red-500">{error}</div>
 
-          <div className="text-sm">
-            New to Airbnb?{' '}
-            <Link
-              className="text-[#FF5A5F] underline underline-offset-4"
-              to="/signup"
-            >
-              Create an Account
-            </Link>
-          </div>
-        </form>
+          <form onSubmit={submitForm} className="grid gap-3">
+            <div>
+              <label className="text-stone-500 text-sm">Email:</label>
+              <input type="text" name="email" className="border w-full p-1" />
+            </div>
+            <div>
+              <label className="text-stone-500 text-sm">Password:</label>
+              <input
+                type="password"
+                name="password"
+                className="border w-full p-1"
+              />
+            </div>
+            <button className=" bg-[#FF5A5F] border text-white p-2 rounded-md w-full">
+              Login
+            </button>
+            <div className="text-red-500">{error}</div>
+
+            <div className="text-sm">
+              New to Airbnb?{' '}
+              <Link
+                className="text-[#FF5A5F] underline underline-offset-4"
+                to="/signup"
+              >
+                Create an Account
+              </Link>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   )
